1d0d0e835718c99d09b66d055baa3cfcfff249f0
seujeanoliveira <seujeanoliveira@gmail.com>
2017-04-24 22:28:23 -0300

    Original Map Brazil SVG

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,24 @@
 (function ($) {
-    $(function () {
+	$(function () {
+		function moveUpestado(thisObject) {
+			thisObject.appendTo(thisObject.parents('svg>g'));
+		}
 
-        $('.mapa-svg-estados').click(function () {
-            $(this).siblings().removeClass('mapa-svg-estados-active');
-            $(this).addClass('mapa-svg-estados-active');
-            $('.class-select').val($(this).attr('id')).trigger('change');
-        })
+		$('.mapa-svg-estados').click(function () {
+			$(this).siblings().removeClass('mapa-svg-estados-active');
+			$(this).addClass('mapa-svg-estados-active');
+			$('.class-select').val($(this).attr('id')).trigger('change');
+			moveUpestado($(this));
+		});
 
-        $('.class-select > option').each(function(){
-            $(this).addClass($(this).attr('value'));
-        });
+		$('.class-select > option').each(function () {
+			$(this).addClass($(this).attr('value'));
+		});
 
-        $('.class-select').change(function() {
-            $('.' + $(this).val() + '-class').siblings().removeClass('mapa-svg-estados-active');
-            $('.' + $(this).val() + '-class').addClass('mapa-svg-estados-active');
-        });
+		$('.class-select').change(function () {
+			$('.' + $(this).val() + '-class').siblings().removeClass('mapa-svg-estados-active');
+			$('.' + $(this).val() + '-class').addClass('mapa-svg-estados-active');
+		});
 
-    });
+	});
 })(jQuery);
